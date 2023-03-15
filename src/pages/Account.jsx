79b75f4a602a0c2e1e3be1ab26d80f6307af72bf
zaki <zikoubouregbi@gmail.com>
2023-03-15 19:10:58 +0100

    save data in firebase

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 
+import Shows from '../components/Shows'
+
 function Account() {
   return (
-    <div>
-      
-    </div>
+    <>
+    <div className='w-full text-white'>
+        <img className='w-full h-[400px] object-cover' src="https://assets.nflxext.com/ffe/siteui/vlv3/5523db5a-e2b2-497f-a88b-61f175c3dbad/8f90a0de-7e17-46e3-8f96-5241b38af33f/DZ-en-20230306-popsignuptwoweeks-perspective_alpha_website_large.jpg" alt="/" />
+
+        <div className='bg-black/60 fixed top-0 left-0 w-full h-[550px]'></div>
+<div className='absolute top-[20%] p-4 md:p-8'>
+    <h1 className='text-3xl md:text-5xl font-bold'>My Shows</h1>
+</div>
+
+ </div>
+<Shows />
+
+ </>
+
   )
 }
+
 
 export default Account
